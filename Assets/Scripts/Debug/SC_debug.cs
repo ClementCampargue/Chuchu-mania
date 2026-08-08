c37a9f9e08cb6076228ea5df7c2c06bb3e00d292
Clément <94358531+ClementCampargue@users.noreply.github.com>
2026-08-08 23:16:00 +0200

    fix stickers

--- Assets/Scripts/Debug/SC_debug.cs
+++ Assets/Scripts/Debug/SC_debug.cs
@@ -1,42 +1,44 @@
 using UnityEngine;
 
+#if UNITY_EDITOR
+
 public class SC_debug : MonoBehaviour
 {
-
     private SC_icecream_eat_system eat;
+
     void Start()
     {
         eat = SC_icecream_eat_system.instance;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
             SC_score.Instance.score = 1000;
             eat.ActivatePowerUpInstant();
-
         }
+
         if (Input.GetKeyDown(KeyCode.R))
         {
             SC_player.instance.Revive();
-
         }
+
         if (Input.GetKeyDown(KeyCode.N))
         {
             SC_phases.instance.NextPhase();
-
         }
+
         if (Input.GetKeyDown(KeyCode.B))
         {
-            Time.timeScale =5;
-
+            Time.timeScale = 5;
         }
+
         if (Input.GetKeyDown(KeyCode.K))
         {
             SC_player.instance.Die();
-
         }
     }
 }
+
+#endif
--- Assets/Scripts/Stickers/SC_StickerSaveSystem.cs
+++ Assets/Scripts/Stickers/SC_StickerSaveSystem.cs
@@ -8,8 +8,13 @@ public class SC_StickerSaveSystem : MonoBehaviour
 
     [Header("Prefabs")]
     public GameObject stickerPrefab;
-    public Transform parent; // canvas container
-    public List<Sprite> sprites; // toutes les sprites possibles
+    public Transform parent;
+
+    [Header("Sticker Database")]
+    public SO_sticker_list stickers;
+
+    [Header("Delete Zone")]
+    public RectTransform deleteZone;
 
     private const string SAVE_KEY = "ui_stickers_save";
 
@@ -35,13 +40,30 @@ public class SC_StickerSaveSystem : MonoBehaviour
         public List<StickerData> stickers = new List<StickerData>();
     }
 
-    void Awake()
+    private void Awake()
     {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         instance = this;
     }
-    void Start()
+
+    private void Start()
     {
-        deleteZone = GameObject.Find("TrashZone").GetComponent<RectTransform>();
+        // Cherche automatiquement la TrashZone si elle n'est pas assignée
+        if (deleteZone == null)
+        {
+            GameObject trashObject = GameObject.Find("TrashZone");
+
+            if (trashObject != null)
+            {
+                deleteZone = trashObject.GetComponent<RectTransform>();
+            }
+        }
+
         Load();
     }
 
@@ -49,78 +71,243 @@ public class SC_StickerSaveSystem : MonoBehaviour
     {
         Save();
     }
-    public RectTransform deleteZone;
+
     public void Save()
     {
-        SC_sticker_UI[] stickers = FindObjectsOfType<SC_sticker_UI>();
+        SC_sticker_UI[] stickerObjects =
+            FindObjectsOfType<SC_sticker_UI>();
+
         StickerSave save = new StickerSave();
 
-        foreach (SC_sticker_UI st in stickers)
+        foreach (SC_sticker_UI stickerUI in stickerObjects)
         {
-            RectTransform rt = st.GetComponent<RectTransform>();
+            if (stickerUI == null)
+                continue;
 
-            // Vérifie si le centre du sticker est dans la TrashZone
-            Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(null, rt.position);
+            RectTransform rt = stickerUI.GetComponent<RectTransform>();
+            Image img = stickerUI.GetComponent<Image>();
 
+            if (rt == null || img == null)
+                continue;
+
+            // Position écran du centre du sticker
+            Vector2 screenPos =
+                RectTransformUtility.WorldToScreenPoint(
+                    null,
+                    rt.position
+                );
+
+            // Si le sticker est dans la TrashZone,
+            // on le détruit et on ne le sauvegarde pas.
             if (deleteZone != null &&
-                RectTransformUtility.RectangleContainsScreenPoint(deleteZone, screenPos))
+                RectTransformUtility.RectangleContainsScreenPoint(
+                    deleteZone,
+                    screenPos
+                ))
             {
-                Debug.Log("Sticker supprimé (SaveSystem)");
-                Destroy(st.gameObject);
-                continue; // Ne pas sauvegarder ce sticker
-            }
+                Debug.Log("Sticker supprimé par le SaveSystem.");
 
-            Image img = st.GetComponent<Image>();
+                Destroy(stickerUI.gameObject);
+                continue;
+            }
 
             StickerData data = new StickerData();
 
-            data.spriteName = img.sprite != null ? img.sprite.name : "";
+            // Nom du sprite
+            data.spriteName =
+                img.sprite != null
+                    ? img.sprite.name
+                    : "";
 
+            // Position UI
             data.posX = rt.anchoredPosition.x;
             data.posY = rt.anchoredPosition.y;
 
+            // Rotation
             data.rotZ = rt.localEulerAngles.z;
 
+            // Scale
             data.scaleX = rt.localScale.x;
             data.scaleY = rt.localScale.y;
 
+            // Ordre dans le Canvas
             data.siblingIndex = rt.GetSiblingIndex();
 
             save.stickers.Add(data);
         }
 
-        PlayerPrefs.SetString(SAVE_KEY, JsonUtility.ToJson(save));
+        string json = JsonUtility.ToJson(save);
+
+        PlayerPrefs.SetString(SAVE_KEY, json);
         PlayerPrefs.Save();
+
+        Debug.Log("Stickers sauvegardés : " + save.stickers.Count);
     }
+
     public void Load()
     {
         if (!PlayerPrefs.HasKey(SAVE_KEY))
+        {
+            Debug.Log("Aucune sauvegarde de stickers trouvée.");
             return;
+        }
+
+        if (stickerPrefab == null)
+        {
+            Debug.LogError("StickerPrefab n'est pas assigné.");
+            return;
+        }
+
+        if (parent == null)
+        {
+            Debug.LogError("Parent n'est pas assigné.");
+            return;
+        }
+
+        if (stickers == null)
+        {
+            Debug.LogError("SO_sticker_list n'est pas assigné.");
+            return;
+        }
 
         string json = PlayerPrefs.GetString(SAVE_KEY);
-        StickerSave save = JsonUtility.FromJson<StickerSave>(json);
+
+        StickerSave save =
+            JsonUtility.FromJson<StickerSave>(json);
+
+        if (save == null || save.stickers == null)
+        {
+            Debug.LogWarning("La sauvegarde des stickers est invalide.");
+            return;
+        }
 
         foreach (StickerData data in save.stickers)
         {
-            GameObject obj = Instantiate(stickerPrefab, parent).transform.GetChild(0).gameObject;
+            // Création du prefab
+            GameObject prefabInstance =
+                Instantiate(stickerPrefab, parent);
 
-            RectTransform rt = obj.GetComponent<RectTransform>();
-            Image img = obj.GetComponent<Image>();
+            // Ton prefab semble contenir le sticker
+            // comme premier enfant.
+            GameObject stickerObject = prefabInstance;
 
-            rt.anchoredPosition = new Vector2(data.posX, data.posY);
-            rt.localEulerAngles = new Vector3(0, 0, data.rotZ);
-            rt.localScale = new Vector3(data.scaleX, data.scaleY, 1);
+            if (prefabInstance.transform.childCount > 0)
+            {
+                stickerObject =
+                    prefabInstance.transform.GetChild(0).gameObject;
+            }
 
-            img.sprite = sprites.Find(s => s.name == data.spriteName);
-            img.maskable = true;
-            obj.transform.SetSiblingIndex(data.siblingIndex);
+            RectTransform rt =
+                stickerObject.GetComponent<RectTransform>();
 
-            obj.GetComponent<SC_sticker_UI>().spawnedSticker = true;
+            Image img =
+                stickerObject.GetComponent<Image>();
+
+            if (rt == null || img == null)
+            {
+                Debug.LogWarning(
+                    "Le sticker chargé n'a pas de RectTransform ou d'Image."
+                );
+
+                Destroy(prefabInstance);
+                continue;
+            }
+
+            // Position
+            rt.anchoredPosition =
+                new Vector2(
+                    data.posX,
+                    data.posY
+                );
+
+            // Rotation
+            rt.localEulerAngles =
+                new Vector3(
+                    0f,
+                    0f,
+                    data.rotZ
+                );
+
+            // Scale
+            rt.localScale =
+                new Vector3(
+                    data.scaleX,
+                    data.scaleY,
+                    1f
+                );
+
+            // Recherche du Sprite dans la liste SO
+            Sprite loadedSprite = FindSprite(data.spriteName);
+
+            if (loadedSprite != null)
+            {
+                img.sprite = loadedSprite;
+                img.maskable = true;
+            }
+            else
+            {
+                Debug.LogWarning(
+                    "Sprite introuvable : " + data.spriteName
+                );
+            }
+
+            // Restaurer l'ordre
+            stickerObject.transform.SetSiblingIndex(
+                data.siblingIndex
+            );
+
+            // Indiquer que le sticker vient d'une sauvegarde
+            SC_sticker_UI stickerUI =
+                stickerObject.GetComponent<SC_sticker_UI>();
+
+            if (stickerUI != null)
+            {
+                stickerUI.spawnedSticker = true;
+            }
         }
+
+        Debug.Log(
+            "Stickers chargés : " +
+            save.stickers.Count
+        );
+    }
+
+    private Sprite FindSprite(string spriteName)
+    {
+        if (string.IsNullOrEmpty(spriteName))
+            return null;
+
+        if (stickers == null ||
+            stickers.stickers == null)
+            return null;
+
+        foreach (SO_Sticker sticker in stickers.stickers)
+        {
+            if (sticker == null)
+                continue;
+
+            if (sticker.sticker_sprite == null)
+                continue;
+
+            if (sticker.sticker_sprite.name == spriteName)
+            {
+                return sticker.sticker_sprite;
+            }
+        }
+
+        return null;
     }
 
     public void Clear()
     {
         PlayerPrefs.DeleteKey(SAVE_KEY);
+        PlayerPrefs.Save();
+
+        Debug.Log("Sauvegarde des stickers supprimée.");
+    }
+
+    public bool HasSave()
+    {
+        return PlayerPrefs.HasKey(SAVE_KEY);
     }
 }
